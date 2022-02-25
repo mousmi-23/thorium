6c0c0d6b91e8db882becf5a88d6c5923e84b106b
mousmi-23 <mousmirahangdale23@gmail.com>
2022-02-25 23:05:35 +0530

    Post- API

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,168 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// You have to write 2 POST apis:
+// Write the api in first project directory (Routes/index.js or routes/route.js)
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+// Problem Statement 1 :
+// NOTE: you must create the players array outside( on the top ) of the api( so that data is maintained across api hits )
+// Your player collection should be an ARRAY of player objects. Each player object should have the following attributes:
+// {
+// "name": "manish",
+// "dob": "1/1/1995",
+// "gender": "male",
+// "city": "jalandhar",
+// "sports": [
+// "swimming"
+// ],
+// "bookings": [
+// ]
+// }
+// e.g. the players array would look like this:
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ],
+           "bookings": [
+               {
+                   "bookingNumber": 1,
+                   "sportId": "",
+                   "centerId": "",
+                   "type": "private",
+                   "slot": '16286598000000',
+                   "bookedOn": '31/08/2021',
+                   "bookedFor": '01/09/2021'
+               },
+               {
+                   "bookingNumber": 2,
+                   "sportId": "",
+                   "centerId": "",
+                   "type": "private",
+                   "slot": '16286518000000',
+                   "bookedOn": '31/08/2001',
+                   "bookedFor": '01/09/2001'
+               },
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+           "bookings": []
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+           "bookings": []
+       },
+   ];
+   router.post('/players', function(req,res) {
+       const playersData = req.body;
+       let playersStatus = null;
+       for (let i=0; i<players.length; i++) {
+           if(players[i].name == playersData.name) {
+           playersStatus = true;
+           break;
+       } else {
+             playersStatus = false;
+       }
+       }
+       if (!playersStatus){
+         players.push(playersData);
+   res.send(players);
+       } else {
+           res.send("Player name is already exist...")
+       }
+}); 
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+    // Write a POST /players api that creates a new player ( that saves a player’s details and doesn’t
+    // allow saving the data of a player with a name that already exists in the data)
 
+    // Problem Statement 2 :
+    // Follow the following structure for a booking resource
+    // {
+    // “bookingNumber”: 1
+    // “sportId": “”,
+    // “centerId: “”,
+    // “type”: “private”,
+    // “slot”: ‘16286598000000’,
+    // “bookedOn”: ’31/08/2021',
+    // “bookedFor”: ’01/09/2021’
+    // }
+    // Write an api that books a slot for a player with relevant details. The api looks like POST
+    // /players/:playerName/bookings/:bookingId
+    // Ensure the below conditions:
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+    // 1. PlayerName and bookingId are path params You have to ensure the playerName received must
+    // exist in the players collection. If the playerName doesn’t exist in the players collection return an error
+    // message that says something relevant about player not being found.
 
+    // 2. For a valid playerName check if the bookingId is already present in the player’s booking. Again,
+    // for a repeated bookingId send an error message conveying the booking was already processed. For
+    // a relevant bookingId(which is new), add the booking object from request body to bookings array.
+    // NOTE: you must create the players array outside( on the top ) of the api( so that data is
+    // maintained across api hits)
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
+router.post('/players/:playerName/booking/:bookingId', (request, response)=>{
+    const playerName = request.params.playerName; 
+    const bookingId = request.params.bookingId; 
+    const bookingData = request.body; 
+    bookingData.bookingNumber = Number(bookingId); 
 
+    let playerNameStatus = "", bookingIdStatus = ""; 
+    
+    for(let i = 0; i < players.length; i++){
+        if(players[i].name == playerName){
+            playerNameStatus = i; 
+            break; 
+        }
+        else{
+            playerNameStatus = null; 
+        }
+    }
+    if(playerNameStatus != null){
+        if(players[playerNameStatus].bookings.length == 0){
+            bookingIdStatus = players[playerNameStatus].bookings;
+        }
+        else{
+            for(let i = 0; i < players[playerNameStatus].bookings.length; i++){
+                if(players[playerNameStatus].bookings[i].bookingNumber == bookingId){
+                    bookingIdStatus = null; 
+                    break; 
+                }
+                else{
+                    bookingIdStatus = players[playerNameStatus].bookings;  
+                }
+            }
+        }
+        if(bookingIdStatus != null){
+            bookingIdStatus.push(bookingData);
+            response.send(players); 
+        }
+        else{
+            response.send("Booking number already exist !"); 
+        }
+    }
+    else{
+        response.send("Player name not found !"); 
+    }
+}); 
 
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
-
-module.exports = router;
+ module.exports = router;
