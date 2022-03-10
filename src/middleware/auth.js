a419d7ca05165c6973572ee55f6f307d6ba9f5e1
mousmi-23 <99881237+mousmi-23@users.noreply.github.com>
2022-03-11 02:45:58 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -31,39 +31,4 @@ const auth = function(req, res, next) {
     next();
 }
 
- module.exports.auth = auth;
-
-
-// const authenticate = function (req, res, next) {
-//     const userId = req.params.userId
-//     const token = req.headers["x-auth-token"];
-  
-//     if (!token) {
-//       return res.send({'status':false, 'msg': "A token is required for authentication"});
-//     } 
-//       try {
-//         const decodedToken = jwt.verify(token, 'functionup-thorium');
-       
-//       } catch(err){
-//         console.log(err)
-//         return res.send({'status': false, 'msg': "Invalid Token"});
-//       }
-      
-//     next();
-//   };
-  
-  
-//   const authorise = function(req, res, next) {
-//     const userId = req.params.userId
-//     const token = req.headers["x-auth-token"];
-//     const decodedToken = jwt.verify(token, 'functionup-thorium');
-//     let userLoggedIn = decodedToken.userId
-//     let userToBeModified = userId
-//     if(userToBeModified != userLoggedIn) {
-//        return res.send({status: false, msg: "User logged is not allowed to modify the requested users data"});
-//     }
-//     next();
-//   };
-  
-//   module.exports.authenticate = authenticate;
-//   module.exports.authorise = authorise
+module.exports.auth = auth
