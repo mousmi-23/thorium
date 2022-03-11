ec17adbd6ab36b962d61057e41376082302b54f0
mousmi-23 <mousmirahangdale23@gmail.com>
2022-03-11 16:00:17 +0530

    Task

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,112 @@
-const UserModel= require("../models/userModel")
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
+const createUser = async function (req, res) {
+  try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    res.status(201).send({ status: true, 'msg': savedData });
+  } catch (error) {
+    console.log("This is the error:", error)
+    res.status(500).send({ status: false, msg: error.message });
+  }
+};
 
+const loginUser = async function (req, res) {
+   try {
+     let userName = req.body.emailId;
+     let password = req.body.password;
 
+     let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.send({
+         status: false,
+         msg: "username or the password is not corerct",
+       });
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "thorium",
+        organisation: "FUnctionUp",
+      },
+      "functionup-thorium"
+      );
+     res.setHeader("x-auth-token", token);
+     res.status(201).send({ status: true, data: token });
+    } catch (error) {
+      res.status(500).send({ status: false, msg: error.message })
+     }
+};
 
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+const getUserData = async function (req, res) {
+   try {
+     let userId = req.params.userId;
+     let userDetails = await userModel.findById(userId);
+     res.status(200).send({ status: true, data: userDetails });
+   } catch (error) {
+     return res.status(500).send({ status: false, msg: error.message })
+   }
+};
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+const updateUser = async function (req, res) {
+   try {
+     let userId = req.params.userId;
+     let userData = req.body;
+     let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+     res.status(200).send({ status: true, data: updatedUser });
+   } catch (error) {
+     res.status(500).send({ status: false, msg: error.message })
+   }
+};
+
+const deleteUser = async function (req, res) {
+  try {
+     let userId = req.params.userId;
+     let userRes = await userModel.findByIdAndUpdate(userId, { isDeleted: true }, { new: true });
+     res.status(200).send({ 'status': true, 'data': userRes });
+   } catch (error) {
+     res.status(500).send({ status: false, msg: error.message })
+   }
+};
+
+const postMessage = async function (req, res) {
+  try {
+     let userId = req.params.userId
+     let message = req.body.message
+
+     let user = await userModel.findById(req.params.userId)
+     if (!user) return res.send({ 'status': false, 'msg': 'No such user exists' })
+
+    let updatedPosts = user.posts
+    updatedPosts.push(message)
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, { posts: updatedPosts }, { new: true })
+    return res.status(200).send({ 'status': true, 'data': updatedUser })
+    } catch (error) {
+     res.status(500).send({ status: false, msg: error.message })
     }
+};
+ let getOtp =async function(req, res) {
+   try {
+     let something = req.body
+     console.log(`body is : ${something}`)
+     var options = {
+       method : "post",
+       url : `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
+       data : something
+     }
+let result = await axios(options)
+console.log(result.data)
+res.status(200).send({msg : result.data})
+   }catch(err) {
+     console.log(err)
+     res.status(500).send({msg : err.message})
+   }
+ }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports.createUser = createUser;
+module.exports.loginUser = loginUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.deleteUser = deleteUser;
+module.exports.postMessage = postMessage
+module.exports.getOtp = getOtp;
--- src/routes/route.js
+++ src/routes/route.js
@@ -17,6 +17,6 @@ router.post("/cowin/getOtp", CowinController.getOtp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
-
+router.get("/cowin/getDistrictId", CowinController.getDistrictId)
 
 module.exports = router;
